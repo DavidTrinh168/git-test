45efeb61a2d29cd810fc3b71a6cdcc71cac17d78
Trinh Ngoc Dai <daingoctrinh72711@gmail.com>
2023-10-24 02:16:40 +0700

    Lab 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $("#mycarousel").carousel({ interval: 4000 });
     $("#carousel-pause").click(function () {
@@ -8,3 +7,24 @@ $(document).ready(function () {
         $("#mycarousel").carousel('cycle');
     });
 });
+
+$(document).ready(function() {
+    const reserveModal = $('#reserveModal');
+    const reserveModalButton = $('#reserveButton');
+    
+    reserveModalButton.click(function() {
+        reserveModal.modal('show');
+    });
+
+});
+
+$(document).ready(function() {
+    const loginModal = $('#loginModal');
+    const loginModalButton = $('#LoginBtn');
+    
+    loginModalButton.click(function() {
+        loginModal.modal('show');
+    });
+    
+});
+
